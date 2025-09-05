b0b87b7d781eab7b5bf257afe0f577c1b60ea710
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-09-05 22:10:47 +0100

    fix member view again

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -59,4 +59,4 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user='Member')
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # Role-based URLs
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
+    path('member-view/', views.member_view, name='member_view'),
 
      # Authentication URLs
     path('register/', views.register, name='register'),
