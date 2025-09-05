39235a5573b01702010cdaa9204848967b81a8fd
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-09-05 21:17:02 +0100

    login logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    register_view,
-    login_view,
-    logout_view
-)
+from .views import list_books, LibraryDetailView, register_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-based view
@@ -16,6 +11,6 @@ urlpatterns = [
 
      # Authentication URLs
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 ]
