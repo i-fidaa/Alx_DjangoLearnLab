7cc2cc9a9e13bc71e394f7866951570db490b116
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-09-05 18:53:06 +0100

    syntax

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
