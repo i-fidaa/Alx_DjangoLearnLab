8b4e59db5a1fcdeede39bf2b5afb8bb14388052e
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-09-05 21:25:30 +0100

    imported views as a module

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-based view
-    path("books/", list_books, name="list_books"),
+    path("books/", views.list_books, name="list_books"),
 
     # Class-based view; expects a library ID
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("library/<int:pk>/", views.libraryDetailView.as_view(), name="library_detail"),
 
      # Authentication URLs
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 ]
