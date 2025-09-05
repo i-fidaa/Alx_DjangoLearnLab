d5955a5fcc4b2501b9c59c06d591cba81bbe13b2
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-09-05 21:30:58 +0100

    implement role-based access control

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     # Class-based view; expects a library ID
     path("library/<int:pk>/", views.libraryDetailView.as_view(), name="library_detail"),
 
+    # Role-based URLs
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+
      # Authentication URLs
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
