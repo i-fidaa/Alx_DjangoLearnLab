ea7eb9af9a9c4ac7a14500d8b2b2d44045ec021b
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-09-27 19:42:55 +0100

    add import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 from .permissions import IsAdminOrReadOnly
